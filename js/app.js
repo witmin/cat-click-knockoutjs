ae6de9a0baa2dce5e514f691f11778121901fd6f
Millie Lin <linxinmin@gmail.com>
2017-11-18 22:02:39 +0800

    Add a list of new cats.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://flikr.com/photos/big..');
+var initialCats = [
+    {
+        clickCount: ko.observable(0),
+        name: ko.observable('Tabby'),
+        imgSrc: ko.observable('img/434164568_fea0ad4013_z.jpg'),
+        imgAttribution: ko.observable('https://flikr.com/photos/big,.'),
+        nicknames: ko.observable(['Tabtab', 'T-B', 'Tobe'])
+    },
+    {
+        clickCount: ko.observable(0),
+        name: ko.observable('Cat 2'),
+        imgSrc: ko.observable('img/22252709_010df3379e_z.jpg'),
+        imgAttribution: ko.observable('https://flikr.com/photos/big,.'),
+        nicknames: ko.observable(['Peggy', 'T-B', 'Tobe'])
+    },
+    {
+        clickCount: ko.observable(0),
+        name: ko.observable('Birdy'),
+        imgSrc: ko.observable('img/1413379559_412a540d29_z.jpg'),
+        imgAttribution: ko.observable('https://flikr.com/photos/big,.'),
+        nicknames: ko.observable(['zzz'])
+    }
+];
 
-    this.nicknames = ko.observable(['Tabtab', 'T-B','Tobe']);
+var Cat = function (data) {
+    this.clickCount = data.clickCount;
+    this.name = data.name;
+    this.imgSrc = data.imgSrc;
+    this.imgAttribution = data.imgAttribution;
+    this.nicknames = data.nicknames;
 
     this.level = ko.computed(function () {
         var level;
         var clicks = this.clickCount();
 
-        if (clicks < 10){
+        if (clicks < 10) {
             level = 'Newborn';
         } else if (clicks < 15) {
             level = 'Infant';
@@ -24,7 +47,15 @@ var Cat = function() {
 
 var ViewModal = function () {
     var self = this;
-    self.currentCat = ko.observable(new Cat());
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function (catItem) {
+        self.catList.push( new Cat (catItem))
+    });
+
+    self.currentCat = ko.observable( this.catList()[0]);
+    ;
     self.incrementCounter = function () {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     }
